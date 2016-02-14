e6dd463781c1d84ccdbc872c45375d8c742ce878
minty93 <aajain93@gmail.com>
2016-02-13 21:30:01 -0500

    Addded initialize method

--- sql_object.rb
+++ sql_object.rb
@@ -16,6 +16,16 @@ class SQLObject
   end
 
   def self.finalize!
+    self.columns.each do |col|
+      define_method("#{col}") {
+        @attributes[col]
+      }
+
+      define_method("#{col}=") { |val|
+        self.attributes
+        @attributes[col] = val
+      }
+    end
   end
 
   def self.table_name=(table_name)
@@ -39,11 +49,19 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    self.class.columns
+    params.each do |attr_name, value|
+      attr_name = attr_name.to_sym
+      if @cols.include?(attr_name)
+        self.send("#{attr_name}=", value)
+      else
+        raise "unknown attribute '#{attr_name}'"
+      end
+    end
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
